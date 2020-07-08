2d470342dce1d006471053ddbfd01fbb39554838
Scott Barclay <scottbarclay.js@gmail.com>
2020-07-08 16:00:54 -0700

    added resume section and rand style fixes

--- jS/app.js
+++ jS/app.js
@@ -1,11 +1,52 @@
 const contactBtn = document.querySelector('.contact-btn');
+const navLinks = document.querySelectorAll('.nav-links li');
+const resume = document.querySelector('.resume-img');
+const resumeBtns = document.querySelectorAll('.resume button');
 
 contactBtn.addEventListener('mouseover', function () {
 	this.style.borderRadius = '17% 83% 16% 84% / 73% 77% 23% 27%';
-	this.style.transition = 'all .6s';
+	this.style.transition = 'all .4s';
 });
 
 contactBtn.addEventListener('mouseleave', function () {
 	this.style.borderRadius = '76% 24% 80% 20% / 32% 25% 75% 68%';
-	this.style.transition = 'all .6s';
+	this.style.transition = 'all .4s';
 });
+
+for (let link of navLinks) {
+	link.addEventListener('click', function () {
+		for (link of navLinks) {
+			link.classList.remove('active');
+		}
+		this.classList.add('active');
+	});
+}
+
+resume.addEventListener('mouseenter', function () {
+	for (let btn of resumeBtns) {
+		btn.classList.add('visible');
+		btn.classList.remove('hidden');
+	}
+	this.classList.add('imgHover');
+	this.classList.remove('imgBlur');
+});
+
+resume.addEventListener('mouseleave', function () {
+	for (let btn of resumeBtns) {
+		btn.classList.remove('visible');
+		btn.classList.add('hidden');
+	}
+	this.classList.remove('imgHover');
+	this.classList.add('imgBlur');
+});
+
+for (let btn of resumeBtns) {
+	btn.addEventListener('mouseover', function () {
+		resume.classList.add('imgHover');
+		resume.classList.remove('imgBlur');
+		for (let btn of resumeBtns) {
+			btn.classList.add('visible');
+			btn.classList.remove('hidden');
+		}
+	});
+}
