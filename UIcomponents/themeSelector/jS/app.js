735904f96b887eb9cb83c90cdff19225a909e94e
Scott Barclay <scottbarclay.js@gmail.com>
2020-07-11 18:17:57 -0700

    added UIcomponents folder

---
+++ UIcomponents/themeSelector/jS/app.js
@@ -0,0 +1,94 @@
+const themes = document.querySelectorAll('.theme-card-head');
+const themeCards = document.querySelectorAll('.theme-card');
+const themeColor = document.querySelectorAll('.color');
+const themeName = document.querySelectorAll('h4');
+const themeBtn = document.querySelector('.theme-btn');
+const themeTxtColor = document.querySelectorAll('.text-color');
+const body = document.querySelector('body');
+let chosenTheme;
+
+// Initial themes
+for (let i = 0; i < themes.length; i++) {
+	themes[i].style.backgroundColor = themeColor[i].innerText;
+	themeColor[i].style.color = themes[i].style.backgroundColor;
+	themeColor[i].style.backgroundColor = themeTxtColor[i].innerText;
+	themeTxtColor[i].style.color = themeTxtColor[i].innerText;
+	themeTxtColor[i].style.backgroundColor = themes[i].style.backgroundColor;
+	themeName[i].style.color = themeTxtColor[i].innerText;
+	// When theme is clicked
+	themeCards[i].addEventListener('click', function () {
+		chosenTheme = themes[i];
+		chosenThemeTxt = themeTxtColor[i];
+		for (let theme of themes) {
+			theme.parentElement.classList.remove('choose');
+		}
+		chosenTheme.parentElement.classList.add('choose');
+		return chosenTheme, chosenThemeTxt;
+	});
+}
+
+// When enter is pressed w/ active theme
+window.addEventListener('keypress', (e) => {
+	if (e.which === 13) {
+		addTheme();
+	}
+});
+
+// When choose theme button is clicked
+themeBtn.addEventListener('click', function () {
+	addTheme();
+});
+
+// When space bar is pressed
+window.addEventListener('keypress', function (e) {
+	let randomTheme = Math.floor(Math.random() * themes.length);
+	if (e.which === 32) {
+		chosenTheme = themes[randomTheme];
+		chosenThemeTxt = themeTxtColor[randomTheme];
+		for (let theme of themeCards) {
+			theme.classList.remove('choose');
+		}
+		addTheme();
+		themeCards[randomTheme].classList.add('choose');
+	}
+});
+
+// Add theme function for when space bar or button is pressed
+let addTheme = () => {
+	body.style.backgroundColor = chosenTheme.style.backgroundColor;
+	body.style.color = chosenThemeTxt.style.color;
+	themeBtn.style.backgroundColor = chosenThemeTxt.style.color;
+	themeBtn.style.color = chosenTheme.style.backgroundColor;
+};
+
+// Edited content
+for (let i = 0; i < themeColor.length; i++) {
+	themeColor[i].addEventListener('blur', () => {
+		themes[i].style.backgroundColor = themeColor[i].innerText;
+		themeColor[i].style.color = themes[i].style.backgroundColor;
+		themeTxtColor[i].style.backgroundColor =
+			themes[i].style.backgroundColor;
+	});
+	themeTxtColor[i].addEventListener('blur', () => {
+		themeTxtColor[i].style.color = themeTxtColor[i].textContent;
+		themeColor[i].style.backgroundColor = themeTxtColor[i].innerText;
+		themeName[i].style.color = themeTxtColor[i].innerText;
+	});
+	themeColor[i].addEventListener('keypress', (e) => {
+		if (e.which === 13) {
+			themes[i].style.backgroundColor = themeColor[i].innerText;
+			themeColor[i].style.color = themes[i].style.backgroundColor;
+			themeTxtColor[i].style.backgroundColor =
+				themes[i].style.backgroundColor;
+			themeColor[i].blur();
+		}
+	});
+	themeTxtColor[i].addEventListener('keypress', (e) => {
+		if (e.which === 13) {
+			themeTxtColor[i].style.color = themeTxtColor[i].textContent;
+			themeColor[i].style.backgroundColor = themeTxtColor[i].innerText;
+			themeName[i].style.color = themeTxtColor[i].innerText;
+			themeTxtColor[i].blur();
+		}
+	});
+}
